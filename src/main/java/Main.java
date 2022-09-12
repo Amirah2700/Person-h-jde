a952a3c4440d61e3b3196e144229ceb38542e2a8
amirah2700 <amir2700@hotmail.com>
2022-09-12 10:10:41 +0200

    tilføjet get og set metode og virker nu

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,9 @@
 public class Main {
 
     public static void main (String[] args){
-        Personer p1 = new Personer(1,72);
-        Personer p2 = new Personer ();
-
+        Personer p1 = new Personer(1.72);
+        Personer p2 = new Personer (1.44);
+        System.out.println(p1.getHøjde());
+        System.out.println(("Ny højde" + " " + p2.getHøjde()));
     }
 }
--- src/main/java/Personer.java
+++ src/main/java/Personer.java
@@ -2,12 +2,27 @@ public class Personer {
 
 
     private double højde;
+    private int alder;
     public Personer(double højde){
         this.højde = højde;
 
     }
 
-    public Personer(int højde, int i) {
+    public Personer() {
+
+    }
+    public double double_getHøjde() {
+        return højde;
+    }
+    public void setHøjde(double højde){
+        this.højde = højde;
+
+    }
+
+    public double getHøjde() {
+        return højde;
+    }
+    public void harFødselsdag(){
 
     }
 }
