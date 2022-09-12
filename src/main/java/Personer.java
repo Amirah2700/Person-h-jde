f623e44e5ef28bb1a5da82f9f528075ed15b57e4
amirah2700 <amir2700@hotmail.com>
2022-09-12 09:24:03 +0200

    Person klasse

---
+++ src/main/java/Personer.java
@@ -0,0 +1,13 @@
+public class Personer {
+
+
+    private double højde;
+    public Personer(double højde){
+        this.højde = højde;
+
+    }
+
+    public Personer(int højde, int i) {
+
+    }
+}
